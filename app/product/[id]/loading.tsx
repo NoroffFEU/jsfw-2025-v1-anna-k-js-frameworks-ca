de42ab510c5f1e7f7b639db4449b04fded738a44
kaijanna <kaijanna@hotmail.com>
2026-03-06 22:16:29 +0100

    added loading state for product page

---
+++ app/product/[id]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <main className="max-w-6xl mx-auto px-4 py-10">
+      <p className="text-sm text-black/60">Loading product...</p>
+    </main>
+  );
+}
